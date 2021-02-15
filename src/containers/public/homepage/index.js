03189039e582c573d5dd082cc5a37df1ca0eea3c
Mike McGrath <mike@mikemcgrathonline.us>
2021-02-15 17:55:40 -0500

    Svg animation setup

--- src/containers/public/homepage/index.js
+++ src/containers/public/homepage/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 import HeroSection from '../../../components/HeroSection'
 import backgroundImage from '../../../assets/images/hero-background.png'
+import { ReactComponent as HeroLogo } from '../../../assets/images/hero-logo.svg'
+
+const Logo = styled(HeroLogo)`
+	height: 125px;
+	width: auto;
+	display: block;
+	margin: 15% auto 0;
+	
+	#title {}
+	
+	#first {}
+	
+	#last {}
+	
+	#left-carret {}
+	
+	#slash {}
+	
+	#right-carret {}
+`
+
 const HomePage = () => {
 	return (
 		<HeroSection image={backgroundImage}>
-			<h1>HomePage</h1>
+			<Logo />
 		</HeroSection>
 	)
 }
