a54de2d96dee76e8207bb5cbb4c58b3e849d090f
Mike McGrath <mike@mikemcgrathonline.us>
2021-02-15 22:21:00 -0500

    Getting there

--- src/layouts/public/navbar/index.js
+++ src/layouts/public/navbar/index.js
@@ -2,7 +2,22 @@ import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 
-const Nav = styled.nav``
+const Nav = styled.nav`
+  position: fixed;
+  height: 55px;
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  z-index: 1000;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background: ${({ theme, transparent }) =>
+    transparent ? "transparent" : theme.colors.white_100};
+  padding: ${({
+    theme: {
+      spacing: { m },
+    },
+  }) => ` 0 ${m}`};
+`
 
 const NavBar = (props) => <Nav>NavBar</Nav>
 
--- src/theme/index.js
+++ src/theme/index.js
@@ -4,6 +4,7 @@ import spacing from "./spacing"
 import typography from "./typography"
 
 const theme = {
+  boxShadow: "2px 3px 4px rgba(0, 0, 0, 0.36)",
   breakpoints,
   colors,
   spacing,
