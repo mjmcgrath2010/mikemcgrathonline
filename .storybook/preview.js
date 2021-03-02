9effabc8face52c62b5f3bfa1bdb0fc1e40566ba
Mike McGrath <mike@mikemcgrathonline.us>
2021-03-02 17:54:33 -0500

    Global Styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
 // .storybook/preview.js
 import { ThemeProvider } from "styled-components"
 import theme from "../src/theme/index"
+import GlobalStyle from "../src/theme/globalStyles"
 
 const withThemeProvider = (Story, context) => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Story {...context} />
     </ThemeProvider>
   )
