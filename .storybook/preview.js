6ab52661e087c262a6c0baa10f4d48f9ac6fbe8c
Mike McGrath <mike@mikemcgrathonline.us>
2021-03-01 22:59:05 -0500

    Hook up theme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
+// .storybook/preview.js
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/theme/index'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+const withThemeProvider=(Story,context)=>{
+  return (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  )
 }
+export const decorators = [withThemeProvider];
