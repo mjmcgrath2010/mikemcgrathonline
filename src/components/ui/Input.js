9805791af007da2ffa0e6f6f71e74b79f1b12c34
Mike McGrath <mike@mikemcgrathonline.us>
2021-03-15 22:02:24 -0400

    Input work

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -2,6 +2,48 @@ import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 
-const Input = (props) => <div></div>
+const StyledInput = styled.input`
+  border: 1px solid ${({ theme }) => theme.colors.purple_100};
+  border-radius: 4px;
+  padding: 0.5em 1em;
+  font-family: ${({ theme }) => theme.fontFamilies.sans_100};
+  outline: none;
+  color: ${({ theme }) => theme.colors.black_300};
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.black_100};
+  }
+`
+
+const Input = ({ onChange, placeholder, type, name }) => {
+  return (
+    <>
+      <StyledInput
+        onChange={onChange}
+        name={name}
+        placeholder={placeholder}
+        type={type}
+      />
+    </>
+  )
+}
+
+Input.defaultProps = {
+  onChange: () => {},
+  name: "",
+  placeholder: "",
+  type: "text",
+}
+
+Input.propTypes = {
+  onChange: PropTypes.func,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.oneOf(["text", "radio", "checkbox", "select"]),
+}
 
 export default Input
