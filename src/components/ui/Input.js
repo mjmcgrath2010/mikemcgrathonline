3d43c83b6c741a8f54eb6fcecd0f35624c0034f6
Mike McGrath <mike@mikemcgrathonline.us>
2021-03-15 22:27:53 -0400

    Input group

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -4,8 +4,11 @@ import styled from "styled-components"
 
 const StyledInput = styled.input`
   border: 1px solid ${({ theme }) => theme.colors.purple_100};
+  width: 90%;
+  margin: 0 auto;
+  font-size: ${({ theme, size }) => theme.fontSizes[size] || theme.fontSizes.m};
   border-radius: 4px;
-  padding: 0.5em 1em;
+  line-height: 2;
   font-family: ${({ theme }) => theme.fontFamilies.sans_100};
   outline: none;
   color: ${({ theme }) => theme.colors.black_300};
@@ -19,31 +22,48 @@ const StyledInput = styled.input`
   }
 `
 
-const Input = ({ onChange, placeholder, type, name }) => {
+const StyledLabel = styled.label`
+  color: ${({ theme }) => theme.colors.black_300};
+  font-size: ${({ theme, size }) => theme.fontSizes[size] || theme.fontSizes.m};
+  font-family: ${({ theme }) => theme.fontFamilies.sans_100};
+  width: 90%;
+  margin: 0 auto;
+  padding: 0.5em 1em;
+  font-family: ${({ theme }) => theme.fontFamilies.sans_100};
+`
+
+const StyledInputGroup = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+`
+
+const Input = ({ onChange, placeholder, type, name, size }) => {
   return (
-    <>
-      <StyledInput
-        onChange={onChange}
-        name={name}
-        placeholder={placeholder}
-        type={type}
-      />
-    </>
+    <StyledInput
+      onChange={onChange}
+      name={name}
+      placeholder={placeholder}
+      type="text"
+      size={size}
+    />
   )
 }
+
+Input.Group = StyledInputGroup
+Input.Label = StyledLabel
 
 Input.defaultProps = {
   onChange: () => {},
   name: "",
   placeholder: "",
-  type: "text",
+  size: "m",
 }
 
 Input.propTypes = {
   onChange: PropTypes.func,
   name: PropTypes.string,
   placeholder: PropTypes.string,
-  type: PropTypes.oneOf(["text", "radio", "checkbox", "select"]),
+  size: PropTypes.oneOf(["s", "m", "l"]),
 }
 
 export default Input
--- src/components/ui/stories/Input.stories.js
+++ src/components/ui/stories/Input.stories.js
@@ -1,16 +1,36 @@
 import React from "react"
+import styled from "styled-components"
 
 import Input from "../Input"
+import Card from "../Card"
 
 export default {
   title: "Input",
   component: Input,
 }
 
+const StyledCard = styled(Card)`
+  width: 400px;
+  height: auto;
+`
 //👇 We create a “template” of how args map to rendering
-const Template = (args) => <Input {...args} />
+const Template = (args) => (
+  <StyledCard>
+    <Input {...args} />
+  </StyledCard>
+)
+const TemplateWithLabel = (args) => (
+  <StyledCard>
+    <Input.Group>
+      <Input.Label>Hello</Input.Label>
+      <Input {...args} />
+    </Input.Group>
+  </StyledCard>
+)
 
 //👇 Each story then reuses that template
 export const Default = Template.bind({})
+export const TextInputWithLabel = TemplateWithLabel.bind({})
 
 Default.args = {}
+TextInputWithLabel.args = {}
